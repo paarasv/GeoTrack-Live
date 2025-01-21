ee45cc78b7178a45be39b42a35804ed62232ef87
paarasv <paras200213@gmail.com>
2025-01-21 11:22:06 +0530

    v4

--- app.js
+++ app.js
@@ -28,6 +28,12 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+});
+
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
